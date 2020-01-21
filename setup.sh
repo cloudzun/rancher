4673294ee28037f76b7cc88ad117b7b73c6cecf2
CloudZun <info@cloudzun.com>
2020-01-21 16:34:57 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 az group create --name microk8s --location southeastasia
-az vm create --resource-group microk8s --name microk8shost --image UbuntuLTS --admin-username chengzh --authentication-type password --admin-password Azure1234567890+ --size  Standard_B4ms --custom-data SS.txt  
+az vm create --resource-group microk8s --name microk8shost --image UbuntuLTS --admin-username chengzh --authentication-type password --admin-password Azure1234567890+ --size  Standard_D4s_v3 --custom-data SS.txt  
 az vm open-port --resource-group microk8s --name microk8shost --port 0-65535 --priority 1001
 
