7d24a575e36c8226a4f5714d2ed2f8f219a54c21
CloudZun <info@cloudzun.com>
2020-01-26 17:59:01 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-az group create --name microk8s --location southeastasia
-az vm create --resource-group microk8s --name microk8shost --image UbuntuLTS --admin-username chengzh --authentication-type password --admin-password Azure1234567890+ --size  Standard_D4s_v3 --custom-data SS.txt  
-az vm open-port --resource-group microk8s --name microk8shost --port 0-65535 --priority 1001
-
+az group create --name rancherlab --location southeastasia
+az vm create --resource-group rancherlab --name node1 --image UbuntuLTS --admin-username chengzh --authentication-type password --admin-password Azure1234567890+ --size  Standard_D4s_v3 --custom-data SS.txt  
+az vm open-port --resource-group rancherlab --name node1 --port 0-65535 --priority 1001
+az vm create --resource-group rancherlab --name node2 --image UbuntuLTS --admin-username chengzh --authentication-type password --admin-password Azure1234567890+ --size  Standard_D4s_v3 --custom-data SS.txt  
+az vm open-port --resource-group rancherlab --name node2 --port 0-65535 --priority 1001
+az vm create --resource-group rancherlab --name node2 --image UbuntuLTS --admin-username chengzh --authentication-type password --admin-password Azure1234567890+ --size  Standard_D4s_v3 --custom-data SS.txt  
+az vm open-port --resource-group rancherlab --name node2 --port 0-65535 --priority 1001
