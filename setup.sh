09738027cd4201d5ec13524af8b9154630355e06
CloudZun <info@cloudzun.com>
2020-01-26 18:24:31 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ az vm create --resource-group rancherlab --name node1 --image UbuntuLTS --admin-
 az vm open-port --resource-group rancherlab --name node1 --port 0-65535 --priority 1001
 az vm create --resource-group rancherlab --name node2 --image UbuntuLTS --admin-username chengzh --authentication-type password --admin-password Azure1234567890+ --size  Standard_D4s_v3 --custom-data SS.txt  
 az vm open-port --resource-group rancherlab --name node2 --port 0-65535 --priority 1001
-az vm create --resource-group rancherlab --name node2 --image UbuntuLTS --admin-username chengzh --authentication-type password --admin-password Azure1234567890+ --size  Standard_D4s_v3 --custom-data SS.txt  
-az vm open-port --resource-group rancherlab --name node2 --port 0-65535 --priority 1001
+az vm create --resource-group rancherlab --name node3 --image UbuntuLTS --admin-username chengzh --authentication-type password --admin-password Azure1234567890+ --size  Standard_D4s_v3 --custom-data SS.txt  
+az vm open-port --resource-group rancherlab --name node3 --port 0-65535 --priority 1001
