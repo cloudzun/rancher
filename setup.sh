f6dad19c793716cfcf71fe4f17991515b46feb89
CloudZun <info@cloudzun.com>
2020-01-27 19:51:44 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 az group create --name rancherlab --location southeastasia
 az vm create --resource-group rancherlab --name rancherhost --public-ip-address-dns-name rancherhost --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_B2ms --custom-data master.txt  
 az vm open-port --resource-group rancherlab --name rancherhost --port 443 --priority 1001
-az vm open-port --resource-group rancherlab --name rancherhost --port 80 --priority 1001
+az vm open-port --resource-group rancherlab --name rancherhost --port 80 --priority 1002
 az vm create --resource-group rancherlab --name node1 --public-ip-address-dns-name chengzhnode1 --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_B4ms --custom-data SS.txt  
 az vm create --resource-group rancherlab --name node2 --public-ip-address-dns-name chengzhnode2 --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_B4ms --custom-data SS.txt  
 az vm create --resource-group rancherlab --name node3 --public-ip-address-dns-name chengzhnode3 --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_B4ms --custom-data SS.txt  
