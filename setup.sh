c85f8630dc570c39b51bf8a07b68271d73a91792
CloudZun <info@cloudzun.com>
2020-01-28 09:27:16 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 az vm create --resource-group rancherlab --name node1 --public-ip-address-dns-name chengzhnode1 --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_B4ms --custom-data SS.txt  
-az vm open-port --resource-group rancherlab --name chengzhnode1 --port 443 --priority 1001
-az vm open-port --resource-group rancherlab --name chengzhnode1 --port 80 --priority 1002
+az vm open-port --resource-group rancherlab --name node1 --port 443 --priority 1001
+az vm open-port --resource-group rancherlab --name node1 --port 80 --priority 1002
 
